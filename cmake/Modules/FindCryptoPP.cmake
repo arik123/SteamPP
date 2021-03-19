01c43393d404d83bd0b8d80008b0d10439bee655
Max <maliarmaximilian@gmail.com>
2021-03-19 13:13:21 +0100

    nieco

--- cmake/Modules/FindCryptoPP.cmake
+++ cmake/Modules/FindCryptoPP.cmake
@@ -19,8 +19,10 @@ FIND_PATH (CRYPTOPP_INCLUDE_DIR
   DOC "CryptoPP include directory")
 
 FIND_LIBRARY (CRYPTOPP_LIBRARY
-  NAMES cryptlib cryptopp
+  NAMES cryptlib cryptopp cryptopp-static
   HINTS ${CRYPTOPP_ROOT_DIR}
   PATH_SUFFIXES lib
   PATHS ${CRYPTOPP_SEARCH_PATHS}
   DOC "CryptoPP release library")
+
+SET (CRYPTOPP_LIBRARIES ${CRYPTOPP_LIBRARY})
