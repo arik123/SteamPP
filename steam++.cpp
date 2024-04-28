ed21630b653387802c44eefad1ee521c09c2688f
Maximilián Maliar <max@maliar.net>
2024-04-29 01:26:35 +0200

    update

--- steam++.cpp
+++ steam++.cpp
@@ -33,13 +33,14 @@ SteamClient::~SteamClient() {
 }
 
 void SteamClient::LogOn(const char* username, const char* password, const unsigned char hash[20], const char* code, SteamID steamID) {
+    //FIXME: OUTDATED
 	if (steamID)
 		cmClient->steamID = steamID;
-	
+
 	CMsgClientLogon logon;
 	logon.set_account_name(username);
 	logon.set_password(password);
-	logon.set_protocol_version(65575);
+	logon.set_protocol_version(65580);
 	if (hash) {
 		logon.set_sha_sentryfile(hash, 20);
 	}
@@ -72,7 +73,7 @@ void SteamClient::SetGamePlayed(int gameID) {
 void SteamClient::SetGamePlayed(std::string name) {
     CMsgClientGamesPlayed changedStatus;
 	changedStatus.add_games_played();
-	changedStatus.mutable_games_played(0)->set_game_id(15190414816125648896);
+	changedStatus.mutable_games_played(0)->set_game_id(15190414816125648896u);
 	changedStatus.mutable_games_played(0)->set_game_extra_info(name);
     cmClient->WriteMessage(EMsg::ClientGamesPlayed, changedStatus);
 }
@@ -83,7 +84,7 @@ void SteamClient::JoinChat(SteamID chat) {
 		chat.instance = static_cast<unsigned>(0x100000 >> 1); // TODO: this should be defined somewhere else
 		chat.type = static_cast<unsigned>(EAccountType::Chat);
 	}
-	
+
 	cmClient->WriteMessage(EMsg::ClientJoinChat, sizeof(MsgClientJoinChat), [&chat](unsigned char* buffer) {
 		auto join_chat = new (buffer) MsgClientJoinChat;
 		join_chat->steamIdChat = chat;
@@ -97,12 +98,12 @@ void SteamClient::LeaveChat(SteamID chat) {
 		chat.instance = static_cast<unsigned>(0x100000 >> 1); // TODO: this should be defined somewhere else
 		chat.type = static_cast<unsigned>(EAccountType::Chat);
 	}
-	
+
 	cmClient->WriteMessage(EMsg::ClientChatMemberInfo, sizeof(MsgClientChatMemberInfo) + 20, [&](unsigned char* buffer) {
 		auto leave_chat = new (buffer) MsgClientChatMemberInfo;
 		leave_chat->steamIdChat = chat;
 		leave_chat->type = static_cast<unsigned>(EChatInfoType::StateChange);
-		
+
 		auto payload = buffer + sizeof(MsgClientChatMemberInfo);
 		*reinterpret_cast<std::uint64_t*>(payload) = cmClient->steamID; // chatter_acted_on
 		*reinterpret_cast<EChatMemberStateChange*>(payload + 8) = EChatMemberStateChange::Left; // state_change
@@ -117,45 +118,45 @@ void SteamClient::SendChatMessage(SteamID chat, const char* message) {
 		chat.instance = static_cast<unsigned>(0x100000 >> 1); // TODO: this should be defined somewhere else
 		chat.type = static_cast<unsigned>(EAccountType::Chat);
 	}
-	
+
 	cmClient->WriteMessage(EMsg::ClientChatMsg, sizeof(MsgClientChatMsg) + std::strlen(message) + 1, [&](unsigned char* buffer) {
 		auto send_msg = new (buffer) MsgClientChatMsg;
 		send_msg->chatMsgType = static_cast<std::uint32_t>(EChatEntryType::ChatMsg);
 		send_msg->steamIdChatRoom = chat;
 		send_msg->steamIdChatter = cmClient->steamID;
-		
+
 		std::strcpy(reinterpret_cast<char*>(buffer + sizeof(MsgClientChatMsg)), message);
 	});
 }
 #pragma endregion groupChat
 void SteamClient::SendPrivateMessage(SteamID user, const char* message) {
 	CMsgClientFriendMsg msg;
-	
+
 	msg.set_steamid(user);
 	msg.set_message(message);
 	msg.set_chat_entry_type(static_cast<google::protobuf::uint32>(EChatEntryType::ChatMsg));
-	
+
 	cmClient->WriteMessage(EMsg::ClientFriendMsg, msg);
 }
 
 void SteamClient::SendTyping(SteamID user) {
 	CMsgClientFriendMsg msg;
-	
+
 	msg.set_steamid(user);
 	msg.set_chat_entry_type(static_cast<google::protobuf::uint32>(EChatEntryType::Typing));
-	
+
 	cmClient->WriteMessage(EMsg::ClientFriendMsg, msg);
 }
 
 void SteamClient::RequestUserInfo(std::size_t count, SteamID users[]) {
 	CMsgClientRequestFriendData request;
-	
+
 	while (count--)
 		request.add_friends(users[count]);
-	
+
 	// TODO: allow custom flags
 	request.set_persona_state_requested(282);
-	
+
 	cmClient->WriteMessage(EMsg::ClientRequestFriendData, request);
 }
 template <typename T>
@@ -165,11 +166,11 @@ void Steam::SteamClient::SendCMsg(T& Proto, EMsg eMsg)
 }
 
 std::size_t SteamClient::connected() {
-	packetLength = 0;	
+	packetLength = 0;
 	cmClient->steamID.ID = 0;
 	cmClient->sessionID = 0;
 	cmClient->encrypted = false;
-	
+
 	return 8;
 }
 
@@ -179,11 +180,11 @@ std::size_t SteamClient::readable(const unsigned char* input) {
 		assert(std::equal(MAGIC, MAGIC + 4, input + 4));
 		return packetLength;
 	}
-	
+
 	if (cmClient->encrypted) {
 		byte iv[16];
 		ECB_Mode<AES>::Decryption(cmClient->sessionKey, sizeof(cmClient->sessionKey)).ProcessData(iv, input, 16);
-		
+
 		auto crypted_data = input + 16;
 		CBC_Mode<AES>::Decryption d(cmClient->sessionKey, sizeof(cmClient->sessionKey), iv);
 		// I don't see any way to get the decrypted size other than to use a string
@@ -202,7 +203,7 @@ std::size_t SteamClient::readable(const unsigned char* input) {
 	} else {
 		ReadMessage(input, packetLength);
 	}
-	
+
 	packetLength = 0;
 	return 8;
 }
@@ -210,7 +211,7 @@ std::size_t SteamClient::readable(const unsigned char* input) {
 void SteamClient::ReadMessage(const unsigned char* data, std::size_t length) {
 	auto raw_emsg = *reinterpret_cast<const std::uint32_t*>(data);
 	auto emsg = static_cast<EMsg>(raw_emsg & ~PROTO_MASK);
-	
+
 	// first figure out the header type
 	if (emsg == EMsg::ChannelEncryptRequest || emsg == EMsg::ChannelEncryptResult) {
 		auto header = reinterpret_cast<const MsgHdr*>(data);
